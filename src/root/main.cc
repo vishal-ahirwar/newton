997cb4914908ec90885180b2f29290cdbdc2c60e
vishal-ahirwar <vishlchoudhry0091@gmail.com>
2022-04-29 20:08:36 +0530

    ./src/root/main.c  updated

--- src/root/main.cc
+++ src/root/main.cc
@@ -1,7 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
-int main(void)
+int main(int argc,char**argv)
 {
-    printf("newton:\n");
+    printf("\n");
+    for(auto i=0;i<argc;++i)
+    {
+        printf("%s ",argv[i]);
+    };
+    printf("\n");
     return 0;
 }
