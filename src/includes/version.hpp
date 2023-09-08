98e84cb926a88d22fc6923f3990973880935920b
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2023-09-08 13:36:02 +0530

    +help wizard added

--- src/includes/version.hpp
+++ src/includes/version.hpp
@@ -1,6 +1,6 @@
 #ifndef __VERSION__NEWTON
 #define __VERSION__NEWTON
-#define VERSION 1.03
+#define VERSION 1.0
 #define COMPANY "Vishal ahirwar"
 #define COPYRIGHT "Copyright(c) 2023 Vishal Ahirwar. All rights reserved."
 #endif
--- src/source/app.cc
+++ src/source/app.cc
@@ -5,7 +5,7 @@ namespace CLI
 {
     int showHelp()
     {
-        printf("version : %.2f\nAuthor: %s\n%s\n[Usage]: \nnewton command project-name\n[command] create, compile, run, help!\n",VERSION,COMPANY,COPYRIGHT);
+        printf("About: \n\tlearning C/C++ and tired of creating files and folder and running .cpp,\n\tcompilling manually again n again\n\ttry this it does a lot for you \n\tlike creating full structured project\n\t which will have cmakefile, good clean folder structure ,.gitignore file, and some commands to \n\tinvest your most of time in learning C++ not creating \n\t new project evry time you wanna to learn a new concept of c/c++ ;)\n\t \n \n\tversion : %.2f\n\tAuthor: %s\n\t%s\n[Usage]: \n\tnewton [command] projectname\n[available commands]\n\tcreate- create a new C/C++ project [newton create projectName]\n\tcompile- compile the project[cd into project directory [newton compile]]\n\trun- run the compiled binary [make sure to compile the project before running this command[newton compile]]\n\thelp-show the help wizard\n\tbuild - compile and run the project with this single command[cd into project dir containing cmakefile.txt [newton build]]\n",VERSION,COMPANY,COPYRIGHT);
         return 0;
     }
     int createApp(const char *argv[])
