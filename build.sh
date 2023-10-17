313e7556f5dadc1e5a275584934a75437c3ae6ce
Vishal Ahirwar <73791462+vishal-ahirwar@users.noreply.github.com>
2023-10-17 16:43:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Clone the project, find CMakeLists.txt and Create a build directory.
-git clone https://github.com/vishal-ahirwar/newton
-cd newton
 cmake -S . -B build
-
+cd build
 # Compile and Install the project
 sudo make install
