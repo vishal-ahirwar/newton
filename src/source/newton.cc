bce99a9fa0cf333ee4595909edf714a85305fae8
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2024-10-08 16:29:01 +0530

    cmake code issue fixed

--- src/source/newton.cc
+++ src/source/newton.cc
@@ -261,10 +261,10 @@ void App::generateCmakeFile(const char *argv)
 #Auto Genrated CMake file by newton CLI
 #Copyright 2023 Vishal Ahirwar. #replace with your copyright notice.
 cmake_minimum_required(VERSION 3.0)
-set(CMAKE_CXX_STANDARD 17))
+set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED True)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -static-libgcc -static-libstdc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")"};
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static"))"};
 		file << cmakeCode << "\n";
 		file << "project(" << argv << ")\n";
 		file << "set(SOURCE ./src/main.cc)#add your additional source file here!\n";
